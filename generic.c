2c3dfa61357243dc559b716cf2608cc76af71f29
linzhenghao233 <1392872316@qq.com>
2023-09-03 22:34:37 +0800

    0903

--- generic.c
+++ generic.c
@@ -4,7 +4,34 @@
 #define RAD_TO_DEG (180 / (4 * atan1(1)))
 
 //泛型平方根函数
-#define SQRT(X) _Generic(x),\
-	long douvle: sqrtl,\
+#define SQRT(X) _Generic((X),\
+	long double: sqrt1,\
 	default: sqrt,\
-	float: sqrt)(x)
+	float: sqrtf)(X)
+
+//泛型正弦函数，角度的单位为度
+#define SIN(X) _Generic((X),\
+	long double: sin1((X) / RAD_TO_DEG),\
+	default: sin((X) / RAD_TO_DEG),\
+	float: sinf((X) / RAD_TO_DEG)\
+)
+
+int main(void) {
+	float x = 45.0f;
+	double xx = 45.0;
+	long double xxx = 45.0L;
+
+	long double y = SQRT(x);
+	long double yy = SQRT(xx);
+	long double yyy = SQRT(xxx);
+	printf("%.17Lf\n", y);	//匹配float
+	printf("%.17Lf\n", yy);	//匹配default
+	printf("%.14Lf\n", yyy);	//匹配long double
+	int i = 45;
+	yy = SQRT(i);	//匹配default
+	printf("%.17Lf\n", yy);
+	yyy = SIN(xxx);	//匹配long double
+	printf("%.17Lf\n", yyy);
+
+	return 0;
+}
